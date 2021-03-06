c22f446298ce052ce7cf20d2540f191319675e99
Geor Kasapidi <geor.kasapidi@icloud.com>
2021-03-06 20:04:56 +0300

    ricardo

--- ContentFeed/ViewController.swift
+++ ContentFeed/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 let demoXML = """
 <Root backgroundColor="#fff" alignItems="center" flexDirection="row">
-    <Image bundleImageName="1.png" contentMode="scaleAspectFill" positionType="absolute" top="0" left="0" right="0" bottom="0" />
+    <Image bundleImageName="1.jpg" contentMode="scaleAspectFill" positionType="absolute" top="0" left="0" right="0" bottom="0" />
     <Text content="Cool text 1" fontSize="40" italic="true" textBackgroundColor="#FF0000" textColor="#FFFFFF" marginLeft="20" flex="1" appearanceDelay="1" />
     <Text content="Cool text 2" fontSize="40" textBackgroundColor="#00FF00" textColor="#FFFFFF" flex="1" positionType="absolute" top="100" right="100" appearanceDelay="2" />
     <Text content="Cool text 3" fontSize="40" textBackgroundColor="#0000FF" textColor="#FFFFFF" flex="1" positionType="absolute" bottom="100" right="100" appearanceDelay="3" />
